35e85f491b1759807d9985216221f8df6d761672
jacknroll <ybairwa786@gmail.com>
2026-06-05 15:25:47 +0530

    started lecture 2

---
+++ lecture2.cpp
@@ -0,0 +1,18 @@
+// in 1979 by bjarne stroustrup
+// extension of c
+
+// c++ used to make fast programs
+// more control over system resources and memory management
+// high performance
+// 2011-c++11
+// 2014-c++14
+// 2017-c++17
+
+// iostream is a file and we will use in our program,improve functionality.
+#include <iostream> //header file.//input output functions provides
+
+int main()
+{
+    cout << "Hello jacknroll";
+    return 0;
+}
