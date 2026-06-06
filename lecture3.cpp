89d8a54003d0517e2d730b7bc0c7f3ff6c9665bb
jacknroll <ybairwa786@gmail.com>
2026-06-06 15:07:29 +0530

    completed lec3

--- lecture3.cpp
+++ lecture3.cpp
@@ -1,1 +1,30 @@
-//
+// high level-just an abstraction over binary numbers 0,1.
+// low level-machine language(understood by hw).
+
+// 1.
+// VARIABLES=containers stores our data(different type of data need diff type of containers).
+// int,float,char,string,double,long
+
+// 2.
+// comments-code or writting text cant be ready by compiler to compile.
+//  sinle line comment
+/*multi line
+comment */
+
+#include <iostream>
+using namespace std;
+
+// This program as created by jack n roll with lot of love
+
+/* This is a
+multiline comment
+you will need in life later
+*/
+
+int main()
+{
+    int sum = 6;
+    cout << sum << endl;
+    cout << "Hello jacknrolll";
+    return 0;
+}
